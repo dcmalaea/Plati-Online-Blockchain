ab26d679d691dc6b46c2725020092bd4f9c480dc
Your Name <you@example.com>
2020-04-26 17:34:29 +0200

    contract modified

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,36 +1,27 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract MyContract {
-  string private name;
-  uint private age;
-  string value;
+
+int value;
 
   constructor() public {
-      value = "myValue";
+      value = 500;
   }
 
-  function setName(string memory newName) public{
-      name = newName;
-  }
 
-  function getName() public view returns (string memory){
-      return name;
-  }
 
-  function setAge(uint newAge) public{
-      age=newAge;
-  }
 
-  function getAge() public view returns (uint){
-      return age;
-  }
-
-  function get() public view returns(string memory){
+  function get() public view returns(int){
       return value;
   }
 
-  function set(string memory _value) public{
-      value=_value;
+  function deposit(int amt) public{
+
+      value = value + amt;
+  }
+  function spend(int amt) public{
+
+      value = value - amt;
   }
 
 }
